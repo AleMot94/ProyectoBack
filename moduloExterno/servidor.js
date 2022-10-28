a105d6d2252ef0b5c7cbe173063d3d63f275576d
AleMot94 <Alector666@gmai.com>
2022-10-28 19:42:22 -0300

    server con express

--- moduloExterno/servidor.js
+++ moduloExterno/servidor.js
@@ -1,15 +1,21 @@
-const http = require("http");
+const express = require("express");
 
-const servidor = http.createServer((peticion, respuesta) => {
-  respuesta.end("todo ok");
+const servidor = express();
+
+servidor.get("/index", (peticion, respuesta) => {
+  respuesta.send("<h1>estas en el index</h1>");
+});
+
+servidor.get("/", (peticion, respuesta) => {
+  respuesta.json("[{esto: esUnObjeto}]");
 });
 
 function conectar(puerto = 0) {
   return new Promise((resolve, reject) => {
-    const servidorConectador = servidor.listen(puerto, (err) => {
-      if (err) reject(err);
-      else resolve(servidorConectador);
+    const conectarServer = servidor.listen(puerto, () => {
+      resolve(conectarServer);
     });
+    conectarServer.on("error", (error) => reject(error));
   });
 }
 
