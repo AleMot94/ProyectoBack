239c9dd7e963746300b2947cd2e8c38d4681d8e5
AleMot94 <Alector666@gmai.com>
2022-11-08 20:45:00 -0300

    express y routers

--- controllers/controladorProductos.js
+++ controllers/controladorProductos.js
@@ -15,5 +15,43 @@ function controladorGetProductos(req, res) {
   res.json(productos);
 }
 
+function controladorGetById(req, res) {
+  const id = req.params.id;
+  const elemBuscado = productos.find((e) => e.id === id);
+  if (!elemBuscado) {
+    res.status(404);
+    res.json({ error: `producto no encontrado id: ${id}` });
+  } else {
+    res.json(elemBuscado);
+  }
+}
+
+function controladorPutById(req, res) {
+  const id = req.params.id;
+  const indexElem = productos.findIndex((e) => e.id === id);
+  if (indexElem === -1) {
+    res.status(404);
+    res.json({ error: `producto no encontrado id: ${id}` });
+  } else {
+    productos[indexElem] = req.body;
+    res.json(req.body);
+  }
+}
+
+function controladorDeleteById(req, res) {
+  const id = req.params.id;
+  const indexElem = productos.findIndex((e) => e.id === id);
+  if (indexElem === -1) {
+    res.status(404);
+    res.json({ error: `producto no encontrado id: ${id}` });
+  } else {
+    const productoBorrado = productos.splice(indexElem, 1);
+    res.json(productoBorrado);
+  }
+}
+
 module.exports.controladorPostProductos = controladorPostProductos;
 module.exports.controladorGetProductos = controladorGetProductos;
+module.exports.controladorGetById = controladorGetById;
+module.exports.controladorPutById = controladorPutById;
+module.exports.controladorDeleteById = controladorDeleteById;
--- routers/routerApi.js
+++ routers/routerApi.js
@@ -2,11 +2,17 @@ const express = require("express");
 const {
   controladorPostProductos,
   controladorGetProductos,
+  controladorGetById,
+  controladorPutById,
+  controladorDeleteById,
 } = require("../controllers/controladorProductos");
 
 const routerApi = express.Router();
 
 routerApi.post("/", controladorPostProductos);
 routerApi.get("/", controladorGetProductos);
+routerApi.get("/:id", controladorGetById);
+routerApi.put("/:id", controladorPutById);
+routerApi.delete("/:id", controladorDeleteById);
 
 exports.routerApi = routerApi;
